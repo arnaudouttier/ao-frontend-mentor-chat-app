4167bbcef6f7b680420e17c72a979081bae12cc7
arnaud.outtier <47449100+arnaudouttier@users.noreply.github.com>
2022-06-03 13:32:11 +0200

    show hide message

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 const messageItems = document.querySelectorAll(".message-item");
+let isRun = true;
 
-messageItems.forEach((messageItem, index) => {
-  if (index > 0) {
-    if (
-      messageItems[index - 1].getAttribute("data-message-type") !==
-      messageItems[index].getAttribute("data-message-type")
-    ) {
-      messageItem.style.marginTop = "16px";
+const showHideMessages = () => {
+  messageItems.forEach((messageItem, index) => {
+    if (isRun) {
+      const interval = setInterval(() => {
+        messageItem.style.opacity = "1";
+        clearInterval(interval);
+      }, 1000 * index);
     }
-  }
-});
+
+    if (!isRun) {
+      const interval = setInterval(() => {
+        messageItem.style.opacity = "0";
+        clearInterval(interval);
+      }, 1000 * index);
+    }
+  });
+  isRun = !isRun;
+};
+
+const verticalSpaceMessageType = () => {
+  messageItems.forEach((messageItem, index) => {
+    messageItem.style.opacity = "0";
+
+    if (index > 0) {
+      if (
+        messageItems[index - 1].getAttribute("data-message-type") !==
+        messageItems[index].getAttribute("data-message-type")
+      ) {
+        messageItem.style.marginTop = "16px";
+      }
+    }
+  });
+};
+
+verticalSpaceMessageType();
+showHideMessages();
+setInterval(showHideMessages, 9000);
